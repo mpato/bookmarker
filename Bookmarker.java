158af8883f235ce49d12d15405af6570be190677
Miguel Silva <msilva@msilva.internal.hq.grupopie.com>
2013-06-27 14:41:02 +0100

    Started adding import

--- Bookmarker.java
+++ Bookmarker.java
@@ -1,12 +1,12 @@
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URLDecoder;
-import java.nio.charset.Charset;
 import java.util.Locale;
+import org.apache.commons.io.IOUtils;
 
 import org.apache.http.ConnectionClosedException;
 import org.apache.http.HttpEntity;
@@ -18,8 +18,6 @@ import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.HttpServerConnection;
 import org.apache.http.HttpStatus;
 import org.apache.http.MethodNotSupportedException;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.DefaultConnectionReuseStrategy;
 import org.apache.http.impl.DefaultHttpResponseFactory;
@@ -40,147 +38,181 @@ import org.apache.http.protocol.ResponseContent;
 import org.apache.http.protocol.ResponseDate;
 import org.apache.http.protocol.ResponseServer;
 import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
-public class Bookmarker {
+public class Bookmarker
+{
+  public static void main(String[] args) throws Exception
+  {
+    /*Thread t = new RequestListenerThread(8085);
+     t.setDaemon(false);
+     t.start();*/
+    if (args.length == 2 && args[0].equals("import"))
+      importFrom(args[1]);
+  }
 
-    public static void main(String[] args) throws Exception 
+  static void importFrom(String fileName)
+  {
+    System.out.println("Importing from " + fileName);
+    String json;
+    FileInputStream inputStream;
+   
+    try {
+      inputStream = new FileInputStream(fileName);
+      try {
+        json = IOUtils.toString(inputStream);
+      } catch (IOException ex) {
+        System.out.println("Error reading import file " + fileName);
+        return;
+      } finally {
+        inputStream.close();
+        System.out.println("File closed");
+      }
+      MarkTree markTree;
+      markTree = new MarkTree();
+      System.out.println("Import: "+ markTree.fromJSON((JSONObject)JSONValue.parse(json)));
+    } catch (FileNotFoundException ex) {
+      System.out.println("Error opening import file " + fileName);
+    } catch (Exception e) {
+      System.out.println("Unknown error while importing file " + fileName);
+    }
+  }
+
+  static class HttpJSONRequestHandler implements HttpRequestHandler
+  {
+    public HttpJSONRequestHandler()
     {
-        Thread t = new RequestListenerThread(8085);
-        t.setDaemon(false);
-        t.start();
+      super();
     }
 
-    static class HttpJSONRequestHandler implements HttpRequestHandler  {
+    public void handle(final HttpRequest request, final HttpResponse response, final HttpContext context) throws HttpException, IOException
+    {
+      String target;
+      String method;
+      HttpEntity entity;
+      byte[] entityContent;
+      StringEntity body;
 
-        public HttpJSONRequestHandler()
-	{
-	    super();
-        }
+      entity = null;
+      entityContent = new byte[0];
+      method = request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH);
+      if (!method.equals("POST") && !method.equals("GET")) {
+        throw new MethodNotSupportedException(method + " method not supported");
+      }
+      target = request.getRequestLine().getUri();
+      if (request instanceof HttpEntityEnclosingRequest) {
+        entity = ((HttpEntityEnclosingRequest) request).getEntity();
+        entityContent = EntityUtils.toByteArray(entity);
+        System.out.println("Incoming entity content (bytes): " + entityContent.length);
+      }
 
-        public void handle(final HttpRequest request,final HttpResponse response, final HttpContext context) throws HttpException, IOException 
-	{
-	    String target;
-            String method; 
-	    HttpEntity entity;
-	    byte[] entityContent;
-	    StringEntity body;
+      System.out.println("Entity: " + new String(entityContent));
+      response.setStatusCode(HttpStatus.SC_OK);
+      body = new StringEntity("this is the answer <?>");
+      response.setEntity(body);
+      System.out.println("Responding ...");
+    }
+  }
 
-	    entity = null;
-	    entityContent = new byte[0];
-	    method = request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH);
-            if (!method.equals("POST") && !method.equals("GET")) {
-                throw new MethodNotSupportedException(method + " method not supported");
-            }
-	    target = request.getRequestLine().getUri();
-            if (request instanceof HttpEntityEnclosingRequest) {
-                entity = ((HttpEntityEnclosingRequest) request).getEntity();
-                entityContent = EntityUtils.toByteArray(entity);
-                System.out.println("Incoming entity content (bytes): " + entityContent.length);
-            }
+  static class RequestListenerThread extends Thread
+  {
+    private final ServerSocket serversocket;
+    private final HttpParams params;
+    private final HttpService httpService;
 
-		System.out.println("Entity: " + new String(entityContent));
-	    response.setStatusCode(HttpStatus.SC_OK);
-	    body = new StringEntity("this is the answer <?>");
-	    response.setEntity(body);
-	    System.out.println("Responding ...");
-        }
+    public RequestListenerThread(int port) throws IOException
+    {
+      this.serversocket = new ServerSocket(port);
+      this.params = new SyncBasicHttpParams();
+      this.params.setIntParameter(CoreConnectionPNames.SO_TIMEOUT, 5000)
+              .setIntParameter(CoreConnectionPNames.SOCKET_BUFFER_SIZE, 8 * 1024)
+              .setBooleanParameter(CoreConnectionPNames.STALE_CONNECTION_CHECK, false)
+              .setBooleanParameter(CoreConnectionPNames.TCP_NODELAY, true)
+              .setParameter(CoreProtocolPNames.ORIGIN_SERVER, "MPATO_BOOKMARKER/0.1");
 
+      // Set up the HTTP protocol processor
+      HttpProcessor httpproc = new ImmutableHttpProcessor(new HttpResponseInterceptor[]{
+        new ResponseDate(),
+        new ResponseServer(),
+        new ResponseContent(),
+        new ResponseConnControl()
+      });
+
+      // Set up request handlers
+      HttpRequestHandlerRegistry reqistry = new HttpRequestHandlerRegistry();
+      reqistry.register("*", new HttpJSONRequestHandler());
+
+      // Set up the HTTP service
+      this.httpService = new HttpService(
+              httpproc,
+              new DefaultConnectionReuseStrategy(),
+              new DefaultHttpResponseFactory(),
+              reqistry,
+              this.params);
     }
 
-    static class RequestListenerThread extends Thread {
+    @Override
+    public void run()
+    {
+      System.out.println("Listening on port " + this.serversocket.getLocalPort());
+      while (!Thread.interrupted()) {
+        try {
+          // Set up HTTP connection
+          Socket socket = this.serversocket.accept();
+          DefaultHttpServerConnection conn = new DefaultHttpServerConnection();
+          System.out.println("Incoming connection from " + socket.getInetAddress());
+          conn.bind(socket, this.params);
 
-        private final ServerSocket serversocket;
-        private final HttpParams params;
-        private final HttpService httpService;
-
-        public RequestListenerThread(int port) throws IOException {
-            this.serversocket = new ServerSocket(port);
-            this.params = new SyncBasicHttpParams();
-            this.params.setIntParameter(CoreConnectionPNames.SO_TIMEOUT, 5000)
-                       .setIntParameter(CoreConnectionPNames.SOCKET_BUFFER_SIZE, 8 * 1024)
-                       .setBooleanParameter(CoreConnectionPNames.STALE_CONNECTION_CHECK, false)
-                       .setBooleanParameter(CoreConnectionPNames.TCP_NODELAY, true)
-                       .setParameter(CoreProtocolPNames.ORIGIN_SERVER, "MPATO_BOOKMARKER/0.1");
-
-            // Set up the HTTP protocol processor
-            HttpProcessor httpproc = new ImmutableHttpProcessor(new HttpResponseInterceptor[] {
-                    new ResponseDate(),
-                    new ResponseServer(),
-                    new ResponseContent(),
-                    new ResponseConnControl()
-            });
-
-            // Set up request handlers
-            HttpRequestHandlerRegistry reqistry = new HttpRequestHandlerRegistry();
-            reqistry.register("*", new HttpJSONRequestHandler());
-
-            // Set up the HTTP service
-            this.httpService = new HttpService(
-                    httpproc,
-                    new DefaultConnectionReuseStrategy(),
-                    new DefaultHttpResponseFactory(),
-                    reqistry,
-                    this.params);
+          // Start worker thread
+          Thread t = new WorkerThread(this.httpService, conn);
+          t.setDaemon(true);
+          t.start();
+        } catch (InterruptedIOException ex) {
+          break;
+        } catch (IOException e) {
+          System.err.println("I/O error initialising connection thread: "
+                  + e.getMessage());
+          break;
         }
+      }
+    }
+  }
 
-        @Override
-        public void run() {
-            System.out.println("Listening on port " + this.serversocket.getLocalPort());
-            while (!Thread.interrupted()) {
-                try {
-                    // Set up HTTP connection
-                    Socket socket = this.serversocket.accept();
-                    DefaultHttpServerConnection conn = new DefaultHttpServerConnection();
-                    System.out.println("Incoming connection from " + socket.getInetAddress());
-                    conn.bind(socket, this.params);
+  static class WorkerThread extends Thread
+  {
+    private final HttpService httpservice;
+    private final HttpServerConnection conn;
 
-                    // Start worker thread
-                    Thread t = new WorkerThread(this.httpService, conn);
-                    t.setDaemon(true);
-                    t.start();
-                } catch (InterruptedIOException ex) {
-                    break;
-                } catch (IOException e) {
-                    System.err.println("I/O error initialising connection thread: "
-                            + e.getMessage());
-                    break;
-                }
-            }
-        }
+    public WorkerThread(final HttpService httpservice, final HttpServerConnection conn)
+    {
+      super();
+      this.httpservice = httpservice;
+      this.conn = conn;
     }
 
-    static class WorkerThread extends Thread {
-
-        private final HttpService httpservice;
-        private final HttpServerConnection conn;
-
-        public WorkerThread(final HttpService httpservice, final HttpServerConnection conn) {
-            super();
-            this.httpservice = httpservice;
-            this.conn = conn;
+    @Override
+    public void run()
+    {
+      System.out.println("New connection thread");
+      HttpContext context = new BasicHttpContext(null);
+      try {
+        while (!Thread.interrupted() && this.conn.isOpen()) {
+          this.httpservice.handleRequest(this.conn, context);
         }
-
-        @Override
-        public void run() {
-            System.out.println("New connection thread");
-            HttpContext context = new BasicHttpContext(null);
-            try {
-                while (!Thread.interrupted() && this.conn.isOpen()) {
-                    this.httpservice.handleRequest(this.conn, context);
-                }
-            } catch (ConnectionClosedException ex) {
-                System.err.println("Client closed connection");
-            } catch (IOException ex) {
-                System.err.println("I/O error: " + ex.getMessage());
-            } catch (HttpException ex) {
-                System.err.println("Unrecoverable HTTP protocol violation: " + ex.getMessage());
-            } finally {
-                try {
-                    this.conn.shutdown();
-                } catch (IOException ignore) {}
-            }
+      } catch (ConnectionClosedException ex) {
+        System.err.println("Client closed connection");
+      } catch (IOException ex) {
+        System.err.println("I/O error: " + ex.getMessage());
+      } catch (HttpException ex) {
+        System.err.println("Unrecoverable HTTP protocol violation: " + ex.getMessage());
+      } finally {
+        try {
+          this.conn.shutdown();
+        } catch (IOException ignore) {
         }
-
+      }
     }
+  }
 
 }
--- MarkTree.java
+++ MarkTree.java
@@ -1,30 +1,144 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 class MarkTree
 {
-    static class Mark
+  public static final int JSON_PARSE_OK = 0;
+  public static final int JSON_PARSE_NO_ROOT = 1;
+  public static final int JSON_PARSE_NO_CHILDREN = 2;
+  public static final int JSON_PARSE_NO_TYPE = 3;
+  public static final int JSON_PARSE_INVALID_TYPE = 4;
+  public static final int JSON_PARSE_NO_NAME = 5;
+  public static final int JSON_PARSE_INVALID_ID = 6;
+  public static final int JSON_PARSE_INVALID_DATE = 7;
+
+  static class Mark
+  {
+    public int id;
+    public String description;
+    public String link;
+    public long dateAdded;
+
+    public int fromJSON(JSONObject jnode)
     {
-	public int id;
-	public String description;
-	public String link;
-	public long dateCreated;
-	
+      Object node;
+      node = jnode.get("id");
+      if (!(node instanceof Number))
+        return JSON_PARSE_INVALID_ID;
+      id = ((Number) node).intValue();
+      node = jnode.get("date_added");
+      if (!(node instanceof Number))
+        return JSON_PARSE_INVALID_DATE;
+      dateAdded = ((Number) node).longValue();
+      link = jnode.get("url").toString();
+      description = jnode.get("name").toString();
+      return JSON_PARSE_OK;
+    }
+  }
+
+  static class MarkCategory
+  {
+    public Map<String, MarkCategory> children;
+    public List<Mark> marks;
+
+    public MarkCategory()
+    {
+      children = new HashMap<String, MarkCategory>();
+      marks = new ArrayList<Mark>();
     }
 
-    static class MarkCategory
+    public int fromJSON(JSONObject jnode)
     {
-	public Map<String, MarkCategory> children;
-	public List<Mark> marks;
-	
-	public MarkCategory()
-	{
-	    children = new HashMap<String, MarkCategory>();
-	    marks = new ArrayList<Mark>;
-	}
+      return fromJSON(jnode, true);
     }
 
-    MarkCategory root;
-    
-    public MarkTree()
+    public int fromJSON(JSONObject jnode, boolean clear)
     {
-	root= new MarkCategory();
+      Map<String, MarkCategory> newChildren;
+      List<Mark> newMarks;
+      Object node;
+      JSONObject jchild;
+      String type, name;
+      JSONArray jchildren;
+      MarkCategory category;
+      Mark mark;
+      int ret;
+
+      if (clear) {
+        newChildren = new HashMap<String, MarkCategory>();
+        newMarks = new ArrayList<Mark>();
+      } else {
+        newChildren = children;
+        newMarks = marks;
+      }
+      node = jnode.get("children");
+      if (node == null || !(node instanceof JSONArray))
+        return JSON_PARSE_NO_CHILDREN;
+      jchildren = (JSONArray) node;
+      for (Object child : jchildren) {
+        if (!(child instanceof JSONObject))
+          continue;
+        jchild = (JSONObject) child;
+        node = jchild.get("type");
+        if (node == null || !(node instanceof String))
+          return JSON_PARSE_NO_TYPE;
+        type = (String) node;
+        if (type.equals("folder")) {
+          node = jchild.get("name");
+          if (node == null || !(node instanceof String))
+            return JSON_PARSE_NO_NAME;
+          name = (String) node;
+          category = new MarkCategory();
+          ret = category.fromJSON(jchild);
+          if (ret != JSON_PARSE_OK)
+            return ret;
+          children.put(name.toLowerCase(), category);
+        } else if (type.equals("url")) {
+          mark = new Mark();
+          ret = mark.fromJSON(jchild);
+          if (ret != JSON_PARSE_OK)
+            return ret;
+          marks.add(mark);
+        } else
+          return JSON_PARSE_INVALID_TYPE;
+      }
+      children = newChildren;
+      marks = newMarks;
+      return JSON_PARSE_OK;
     }
+  }
+
+  public MarkCategory roots;
+
+  public MarkTree()
+  {
+    roots = new MarkCategory();
+  }
+
+  public int fromJSON(JSONObject top)
+  {
+    MarkCategory newRoots;
+    Object node;
+    JSONObject jnode;
+    int ret;
+    node = top.get("roots");
+    if (node == null || !(node instanceof JSONObject))
+      return JSON_PARSE_NO_ROOT;
+    newRoots = new MarkCategory();
+    jnode = (JSONObject) node;
+    for (Object child : jnode.entrySet()) {
+      if (!(child instanceof JSONObject))
+        continue;
+      ret = newRoots.fromJSON((JSONObject) child, false);
+      if (ret != JSON_PARSE_OK)
+        return ret;
+    }
+    roots = newRoots;
+    return JSON_PARSE_OK;
+  }
 }
